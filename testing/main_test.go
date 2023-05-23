73a0d6f7689c60d807772a35713021408fc8bc73
Jose <jose.osorio.herrera@gmail.com>
2023-05-23 00:38:47 -0400

    uso de coverprofile en unit test

--- testing/main_test.go
+++ testing/main_test.go
@@ -25,3 +25,35 @@ func TestSum(t *testing.T) {
 		}
 	}
 }
+
+func TestMayor(t *testing.T) {
+	tables := []struct {
+		a int
+		b int
+		c int
+	}{
+		{4, 2, 4},
+		{6, 2, 6},
+		{6, 9, 9},
+	}
+
+	for _, item := range tables {
+		mayor := GetMayor(item.a, item.b)
+		if mayor != item.c {
+			t.Errorf("GetMayor incorrecto obtiene %d y se espera %d", mayor, item.c)
+		}
+	}
+
+}
+
+/*
+	A tener en cuenta
+	go test -coverprofile=coverage.out
+	permite conocer que parte del codigo esta testeado
+
+	Para leer las metricas de forma legible
+	go tool cover -func=coverage.out
+	Resumen en la terminal
+	go tool cover -html=coverage.out
+	Resumen en el navegador
+*/
